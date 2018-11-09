62f31b84e5207016e35d85f5c40415f76e24ddee
B106Roger <imbigking12@gmail.com>
2018-11-10 00:38:07 +0800

    login的樣板有部分無法正常顯示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,34 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- start新加的-->
+    <link rel="shortcut icon" type="image/png" href="{{asset('images/icon/favicon.ico')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/metisMenu.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/slicknav.min.css')}}">
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="{{asset('css/typography.css')}}">
+    <link rel="stylesheet" href="{{asset('css/default-css.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+    <!-- modernizr css -->
+    <script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
+    <!-- end新加的-->
 </head>
 <body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- preloader area start -->
+<div id="preloader">
+    <div class="loader"></div>
+</div>
+<!-- preloader area end -->
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
@@ -76,5 +102,35 @@
             @yield('content')
         </main>
     </div>
+<!-- 新加的-->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- jquery latest version -->
+    <script src="{{asset('js/vendor/jquery-2.2.4.min.js')}}"></script>
+    <!-- bootstrap 4 js -->
+    <script src="{{asset('js/popper.min.js')}}"></script>
+
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/metisMenu.min.js')}}"></script>
+    <script src="{{asset('js/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{asset('js/jquery.slicknav.min.js')}}"></script>
+
+    <!-- start chart js -->
+    <script src={{asset('js/Chart.min.js')}}></script>
+    <!-- start highcharts js -->
+    <script src={{asset('js/highcharts.js')}}></script>
+    <!-- start zingchart js -->
+    <script src={{asset('js/zingchart.min.js')}}></script>
+    <script>
+        zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+        ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
+    </script>
+    <!-- all line chart activation -->
+    <script src="{{asset('js/line-chart.js')}}"></script>
+    <!-- all pie chart -->
+    <script src="{{asset('js/pie-chart.js')}}"></script>
+    <!-- others plugins -->
+    <script src="{{asset('js/plugins.js')}}"></script>
+    <script src="{{asset('js/scripts.js')}}"></script>
+<!-- end新加的-->
 </body>
 </html>
