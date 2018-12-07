a5bbcbe9c4a55215d1b82dcaa1f4a4da052ff6e4
LimaoIsMe <hi@limaois.me>
2018-12-08 00:23:08 +0800

    I hate this hellful world

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 
 
@@ -24,173 +24,161 @@ Route::get('item/destroy/{id}',function(){})->name('item.destroy');
 
 Route::middleware('auth','is_admin')->group(function (){
 
-    Route::middleware('verified')->group(function (){
+  Route::middleware('verified')->group(function (){
 
-        Route::get('/', 'ProductController@index')->name('dashboard.index');
+    Route::get('/', 'ProductController@index')->name('dashboard.index');
 
-        // Products CUD
+    // Products CUD
 
-        Route::post('products/store','ProductController@store')->name('products.store');
+    Route::post('products/store','ProductController@store')->name('products.store');
 
-        Route::get('products/edit/{product}','ProductController@edit')->name('products.edit');
+    Route::get('products/edit/{product}','ProductController@edit')->name('products.edit');
 
-        Route::patch('products/update/{product}','ProductController@update')->name('products.update');
+    Route::patch('products/update/{product}','ProductController@update')->name('products.update');
 
-        Route::delete('products/destroy/{product}','ProductController@destroy')->name('products.destroy');
+    Route::delete('products/destroy/{product}','ProductController@destroy')->name('products.destroy');
 
-        Route::get('products/create','ProductController@create')->name('products.create');
+    Route::get('products/create','ProductController@create')->name('products.create');
 
-        //Messages CUD
+    //Messages CUD
 
-        Route::get('messages/create','MessageController@create')->name('messages.create');
+    Route::get('messages/create','MessageController@create')->name('messages.create');
 
-        Route::post('messages/store','MessageController@store')->name('messages.store');
+    Route::post('messages/store','MessageController@store')->name('messages.store');
 
-        Route::get('messages/edit/{message}','MessageController@edit')->name('messages.edit');
+    Route::get('messages/edit/{message}','MessageController@edit')->name('messages.edit');
 
-        Route::patch('messages/update/{message}','MessageController@update')->name('messages.update');
+    Route::patch('messages/update/{message}','MessageController@update')->name('messages.update');
 
-        Route::delete('messages/destroy/{message}','MessageController@destroy')->name('messages.destroy');
+    Route::delete('messages/destroy/{message}','MessageController@destroy')->name('messages.destroy');
 
-        //Event CUD
+    //Event CUD
 
-        Route::get('events/create','EventController@create')->name('events.create');
+    Route::get('events/create','EventController@create')->name('events.create');
 
-        Route::post('events/store','EventController@store')->name('events.store');
+    Route::post('events/store','EventController@store')->name('events.store');
 
-        Route::get('events/edit/{event}','EventController@edit')->name('events.edit');
+    Route::get('events/edit/{event}','EventController@edit')->name('events.edit');
 
-        Route::post('events/update/{event}','EventController@update')->name('events.update');
+    Route::post('events/update/{event}','EventController@update')->name('events.update');
 
-        Route::delete('events/destroy/{event}','EventController@destroy')->name('events.destroy');
+    Route::delete('events/destroy/{event}','EventController@destroy')->name('events.destroy');
 
-        //Bonus C
+    //Bonus C
 
-        Route::get('bonuses/create','BonusController@create')->name('bonuses.create');
+    Route::get('bonuses/create','BonusController@create')->name('bonuses.create');
 
-        Route::post('bonuses/store','BonusController@store')->name('bonuses.store');
+    Route::post('bonuses/store','BonusController@store')->name('bonuses.store');
 
-        //Sales C
+    //Sales C
 
-        Route::get('sales/create','SaleController@create')->name('sales.create');
+    Route::get('sales/create','SaleController@create')->name('sales.create');
 
-        Route::post('sales/store','SaleController@store')->name('sales.store');
+    Route::post('sales/store','SaleController@store')->name('sales.store');
 
-        //Order C
+    //Order C
 
-        Route::get('orders/edit/{order}','OrderController@edit')->name('orders.edit');
+    Route::get('orders/edit/{order}','OrderController@edit')->name('orders.edit');
 
-        Route::patch('orders/update/{order}','OrderController@update')->name('orders.update');
+    Route::patch('orders/update/{order}','OrderController@update')->name('orders.update');
 
-        //Categories CUD
+    //Categories CUD
 
-        Route::get('categories/create','CategoryController@create')->name('categories.create');
+    Route::get('categories/create','CategoryController@create')->name('categories.create');
 
-        Route::post('categories/store','CategoryController@store')->name('categories.store');
+    Route::post('categories/store','CategoryController@store')->name('categories.store');
 
-        Route::get('categories/edit/{category}','CategoryController@edit')->name('categories.edit');
+    Route::get('categories/edit/{category}','CategoryController@edit')->name('categories.edit');
 
-        Route::patch('categories/update/{category}','CategoryController@update')->name('categories.update');
+    Route::patch('categories/update/{category}','CategoryController@update')->name('categories.update');
 
-        Route::delete('categories/destroy/{category}','CategoryController@destroy')->name('categories.destroy');
+    Route::delete('categories/destroy/{category}','CategoryController@destroy')->name('categories.destroy');
 
-        //Subcategory CUD
+    //Subcategory CUD
 
-        Route::get('categories/subcategories/create/{category}','SubcategoryController@create')->name('subcategories.create');
+    Route::get('categories/subcategories/create/{category}','SubcategoryController@create')->name('subcategories.create');
 
-        Route::post('categories/subcategories/store','SubcategoryController@store')->name('subcategories.store');
+    Route::post('categories/subcategories/store','SubcategoryController@store')->name('subcategories.store');
 
-        Route::get('categories/subcategories/edit/{subcategory}','SubcategoryController@edit')->name('subcategories.edit');
+    Route::get('categories/subcategories/edit/{subcategory}','SubcategoryController@edit')->name('subcategories.edit');
 
-        Route::patch('categories/subcategories/update/{subcategory}','SubcategoryController@update')->name('subcategories.update');
+    Route::patch('categories/subcategories/update/{subcategory}','SubcategoryController@update')->name('subcategories.update');
 
-        Route::delete('subcategories/destroy/{subcategory}','SubcategoryController@destroy')->name('subcategories.destroy');
+    Route::delete('subcategories/destroy/{subcategory}','SubcategoryController@destroy')->name('subcategories.destroy');
 
-        //Tag CUD
+    //Tag CUD
 
-        Route::get('tags/create','TagController@create')->name('tags.create');
+    Route::get('tags/create','TagController@create')->name('tags.create');
 
-        Route::get('tags/edit/{tag}','TagController@edit')->name('tags.edit');
+    Route::get('tags/edit/{tag}','TagController@edit')->name('tags.edit');
 
-        Route::post('tags/store','TagController@store')->name('tags.store');
+    Route::post('tags/store','TagController@store')->name('tags.store');
 
-        Route::patch('tags/update/{tag}','TagController@update')->name('tags.update');
+    Route::patch('tags/update/{tag}','TagController@update')->name('tags.update');
 
-        Route::delete('tags/destroy/{tag}','TagController@destroy')->name('tags.destroy');
+    Route::delete('tags/destroy/{tag}','TagController@destroy')->name('tags.destroy');
 
-        //
-    });
-    //MemberSerach
+    //
+  });
+  //MemberSerach
 
-    Route::get('user/index','HomeController@search')->name('user.index');
+  Route::get('user/index','HomeController@search')->name('user.index');
 
-    Route::get('user/show/{user}','HomeController@show')->name('user.show');
+  Route::get('user/show/{user}','HomeController@show')->name('user.show');
 
-    //User RU
+  //User RU
 
-    Route::get('user/edit','HomeController@edit')->name('user.edit');
+  Route::get('user/edit','HomeController@edit')->name('user.edit');
 
-    Route::post('user/update/{user}','HomeController@update')->name('user.update');
+  Route::post('user/update/{user}','HomeController@update')->name('user.update');
 
-    //Product R
+  //Product R
 
-    Route::get('products','ProductController@index')->name('products.index');
+  Route::get('products','ProductController@index')->name('products.index');
 
-    //message R
+  //message R
 
-    Route::get('messages','MessageController@index')->name('messages.index');
+  Route::get('messages','MessageController@index')->name('messages.index');
 
-    //Event R
+  //Event R
 
-    Route::get('events','EventController@index')->name('events.index');
+  Route::get('events','EventController@index')->name('events.index');
 
-    //Bonus R
+  //Bonus R
 
-    Route::get('bonuses','BonusController@index')->name('bonuses.index');
+  Route::get('bonuses','BonusController@index')->name('bonuses.index');
 
-    //Sale R
+  //Sale R
 
-    Route::get('sales','SaleController@index')->name('sales.index');
+  Route::get('sales','SaleController@index')->name('sales.index');
 
-    //Order R
+  //Order R
 
-    Route::get('orders','OrderController@index')->name('orders.index');
+  Route::get('orders','OrderController@index')->name('orders.index');
 
-    //Category R
+  //Category R
 
-    Route::get('categories','CategoryController@index')->name('categories.index');
+  Route::get('categories','CategoryController@index')->name('categories.index');
 
-    //Tag R
+  //Tag R
 
-    Route::get('tags','TagController@index')->name('tags.index');
+  Route::get('tags','TagController@index')->name('tags.index');
 
-    Route::get('messages','MessageController@index')->name('messages.index');
+  Route::get('messages','MessageController@index')->name('messages.index');
 
-    //Event R
+  //Event R
 
-    Route::get('events','EventController@index')->name('events.index');
+  Route::get('events','EventController@index')->name('events.index');
 
-    //Bonus R
+  //Bonus R
 
-    Route::get('bonuses','BonusController@index')->name('bonuses.index');
+  Route::get('bonuses','BonusController@index')->name('bonuses.index');
 
-Auth::routes();
+  Auth::routes();
 
-    Route::get('orders','OrderController@index')->name('orders.index');
+  Route::get('orders','OrderController@index')->name('orders.index');
 
-    //Category R
+  //Category R
 
-    Route::get('mail/notify/order3','MailController@index3')->name('mails.index3');
-
-Route::get('abc',function (){
-    $order = \App\Order::create([
-        'state' => 1,
-        'message' => '4fgsdfg',
-        'products' => json_encode([3=>3]),
-        'receiver' => 'asdf',
-        'member_id' => 1,
-
-    ]);
-    dd($order->ship_method);
+  Route::get('mail/notify/order3','MailController@index3')->name('mails.index3');
 });
-
